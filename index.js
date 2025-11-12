25ccbbcc8fb8b5a1152897c3f96c34c51c4d88ec
subhrotoBarmon <pappubarmon62@gmail.com>
2025-11-12 16:12:23 +0600

    commit

--- index.js
+++ index.js
@@ -120,10 +120,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
