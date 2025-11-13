98ac5d143d32c32f40f10a0f160851e21d7184ff
subhrotoBarmon <pappubarmon62@gmail.com>
2025-11-13 16:55:29 +0600

    final commit

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ app.use(express.json());
 async function run() {
   try {
     // Connect the client to the server
-    await client.connect();
+    // await client.connect();
     // Database
     let db=client.db("billManagement_db");
     let myBillCollection=db.collection("bills");
     let myUserCollection=db.collection('addBills');
     let myPayBillCollection=db.collection('payBills');
 
-        // Add Bills API
+      // Add Bills API
 
     app.post('/addBills',async(req,res)=>{
        let newUser=req.body;     
@@ -112,7 +112,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -120,6 +120,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send("server is running")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
